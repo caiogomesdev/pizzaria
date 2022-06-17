483abae9edb8fb8b7090d186b994098ff644e072
caiogomesdev <caiogame100@gmail.com>
2022-06-17 17:18:36 -0300

    configure middleware express-async-errors on server

--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -1,9 +1,24 @@
-import express from 'express';
+import express, { Request, Response } from "express";
+import 'express-async-errors'
+
 import { router } from './routes'
 
 const app = express();
 app.use(express.json());
 app.use(router);
+
+app.use((err: Error, req: Request, res: Response) => {
+  if (err instanceof Error) {
+    return res.status(400).json({
+      status: err.name,
+      error: err.message
+    })
+  }
+  return res.status(500).json({
+    status: 'Error',
+    error: 'Internal Server Error'
+  })
+})
 
 const PORT = process.env.PORT || 3333
 app.listen(PORT, () => console.log(`Rodando na porta ${PORT}!!!`));
