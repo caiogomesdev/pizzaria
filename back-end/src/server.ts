2520c4ec1d3f226daeced9528442ec4191f7141f
caiogomesdev <caiogame100@gmail.com>
2022-06-17 17:21:28 -0300

    configure middleware cors on server

--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -1,13 +1,17 @@
 import express, { Request, Response } from "express";
-import 'express-async-errors'
+import 'express-async-errors';
+import cors from 'cors';
 
-import { router } from './routes'
+import { router } from './routes';
 
 const app = express();
+
 app.use(express.json());
+app.use(cors());
+
 app.use(router);
 
-app.use((err: Error, req: Request, res: Response) => {
+app.use((err: Error, _req: Request, res: Response) => {
   if (err instanceof Error) {
     return res.status(400).json({
       status: err.name,
@@ -18,7 +22,7 @@ app.use((err: Error, req: Request, res: Response) => {
     status: 'Error',
     error: 'Internal Server Error'
   })
-})
+});
 
 const PORT = process.env.PORT || 3333
 app.listen(PORT, () => console.log(`Rodando na porta ${PORT}!!!`));
