0d2ae054d2a5f4add8b5723e4a10241c96d181cc
caiogomesdev <caiogame100@gmail.com>
2022-06-17 19:21:21 -0300

    add user controller

---
+++ back-end/src/controllers/user/create-user.controller.ts
@@ -0,0 +1,14 @@
+import { Request, Response } from 'express';
+import { CreateUserService } from '../../services/user'
+
+class CreateUserController {
+  async handle(req: Request, res: Response): Promise<Response> {
+    const { name, email, password } = req.body;
+    const createUserService = new CreateUserService();
+    const user = createUserService.execute({ name, email, password });
+
+    return res.json(user);
+  }
+}
+
+export const createUserController = new CreateUserController();
