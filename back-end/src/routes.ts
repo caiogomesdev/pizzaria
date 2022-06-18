2f58185b623dcbdd068803a3b2f218d9e4b8dbb5
caiogomesdev <caiogame100@gmail.com>
2022-06-18 14:08:36 -0300

    add session route

--- back-end/src/routes.ts
+++ back-end/src/routes.ts
@@ -1,7 +1,9 @@
 import { Router } from 'express'
-import { createUserController } from "./controllers/user";
-const router = Router()
+import { createUserController, authenticationUserController } from "./controllers/user";
+
+const router = Router();
 
 router.post('/users', createUserController.handle);
+router.post('/session', authenticationUserController.handle);
 
 export { router };
