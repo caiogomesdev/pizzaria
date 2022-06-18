8ec2f9d2f7aae17212c952dc0bfea9126cc6fb13
caiogomesdev <caiogame100@gmail.com>
2022-06-18 14:08:14 -0300

    add authentication-user-controller

---
+++ back-end/src/controllers/user/authentication-user.controller.ts
@@ -0,0 +1,15 @@
+import { Request, Response } from 'express';
+import { AuthenticationUserService } from '../../services/user';
+
+class AuthenticationUserController {
+  async handle(req: Request, res: Response) {
+    const { email, password } = req.body;
+    const authenticationUserService = new AuthenticationUserService();
+
+    const auth = await authenticationUserService.execute({ email, password });
+
+    return res.json(auth);
+  }
+}
+
+export const authenticationUserController = new AuthenticationUserController();
