b44f1586dc4a522531690fb13bab2f9615c85a72
caiogomesdev <caiogame100@gmail.com>
2022-06-25 16:56:51 -0300

    remove console

--- back-end/src/controllers/order/close-order.controller.ts
+++ back-end/src/controllers/order/close-order.controller.ts
@@ -3,9 +3,7 @@ import { CloseOrderService } from '../../services/order';
 
 class CloseOrderController {
   async handle(req: Request, res: Response): Promise<Response> {
-    console.log('Entrou');
     const { orderId } = req.params;
-    console.log(orderId);
     const closeOrderService = new CloseOrderService();
     const result = await closeOrderService.execute({ orderId });
     return res.json(result);
