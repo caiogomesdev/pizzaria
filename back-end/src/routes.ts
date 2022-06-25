d639c16aaac554d0787aaf251a38ed6339d85bf7
caiogomesdev <caiogame100@gmail.com>
2022-06-25 13:26:41 -0300

    add listByCategoryController on routes

--- back-end/src/routes.ts
+++ back-end/src/routes.ts
@@ -1,22 +1,41 @@
+import { Router } from 'express';
 import multer from 'multer';
-import { Router } from 'express'
 import uploadConfig from './configs/multer';
 import { isAuthenticated } from './middlewares';
-import { createUserController, authenticationUserController, detailUserController } from './controllers/user';
-import { createCategoryController, listCategoryController } from './controllers/category';
-import { createProductController } from './controllers/product';
+import {
+  createUserController,
+  authenticationUserController,
+  detailUserController,
+} from './controllers/user';
+import {
+  createCategoryController,
+  listCategoryController,
+} from './controllers/category';
+import {
+  createProductController,
+  listByCategoryController,
+} from './controllers/product';
 
 const router = Router();
-const upload = multer(uploadConfig.upload('uploads'))
+const upload = multer(uploadConfig.upload('uploads'));
 
 router.post('/users', createUserController.handle);
 router.post('/session', authenticationUserController.handle);
 router.get('/me', isAuthenticated, detailUserController.handle);
 
-router.post("/category", isAuthenticated, createCategoryController.handle);
-router.get("/category", isAuthenticated, listCategoryController.handle);
+router.post('/category', isAuthenticated, createCategoryController.handle);
+router.get('/category', isAuthenticated, listCategoryController.handle);
 
-router.post("/product", isAuthenticated, upload.single('file'), createProductController.handle);
-
+router.post(
+  '/product',
+  isAuthenticated,
+  upload.single('file'),
+  createProductController.handle
+);
+router.get(
+  '/category/product',
+  isAuthenticated,
+  listByCategoryController.handle
+);
 
 export { router };
