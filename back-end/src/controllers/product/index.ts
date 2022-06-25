582b8a764b339c545ee23458ad6dee96ce607eb0
caiogomesdev <caiogame100@gmail.com>
2022-06-25 13:09:11 -0300

    add listByCategoryController

--- back-end/src/controllers/product/index.ts
+++ back-end/src/controllers/product/index.ts
@@ -1,1 +1,2 @@
 export * from './create-product.controller';
+export * from './list-by-category.controller';
---
+++ back-end/src/controllers/product/list-by-category.controller.ts
@@ -0,0 +1,12 @@
+import { Request, Response} from 'express';
+import { ListByCategoryService } from '../../services/product'
+class ListByCategoryController {
+  async handle(req: Request, res: Response): Promise<Response> {
+    const categoryId = req.query.categoryId as string;
+    const listByCategory = new ListByCategoryService();
+    const result = await listByCategory.execute({ categoryId });
+    return res.json(result);
+  }
+}
+
+export const listByCategoryController = new ListByCategoryController();
